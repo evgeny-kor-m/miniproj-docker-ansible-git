ab6b97a08860348c8ba03999a1ce004cf67f6e61
evgeny-kor-m <evgeny.korchev@gmail.com>
2026-03-12 16:59:42 +0200

    fffff

--- app/src/helper.py
+++ app/src/helper.py
@@ -63,8 +63,6 @@ class pgdb:
             self.cur.execute(f"SELECT nextval('{sequence_name}');")
             next_value = self.cur.fetchone()[0]
 
-            logger.info(f">>>>>>>>>>>>>>>>>> next_value : {next_value} , sequence_name : {sequence_name}")
-
             with self.conn.cursor() as cur:
                 cur.execute("""
                     INSERT INTO POSTGRES_TABLE (RLO, USERNAME, PASSWORD, EMAIL)
