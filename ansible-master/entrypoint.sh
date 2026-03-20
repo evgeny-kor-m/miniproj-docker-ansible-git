a1d022e882a641ac4af60029947565a97203b4cf
evgeny-kor-m <evgeny.korchev@gmail.com>
2026-03-20 14:03:13 +0200

    fffff

--- ansible-master/entrypoint.sh
+++ ansible-master/entrypoint.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+
+
 chown -R ansible:ansible /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
 chmod 600 /home/ansible/.ssh/id_rsa
+
+
+echo "creating mini-project"
+mkdir -p /home/ansible/mini-project
+
+ssh-keyscan -H github.com >> /home/ansible/.ssh/known_hosts
+
+export GIT_SSH_COMMAND="ssh -i /home/ansible/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
+
+echo "Testing SSH connection to GitHub..."
+ssh -T -i /home/ansible/.ssh/id_ed25519 -o StrictHostKeyChecking=no git@github.com || true
+
+echo "Cloning repository $REPO_URL ..."
+git clone $REPO_URL /home/ansible/mini-project
+
+echo "Running install playbook..."
+ansible-playbook -i /home/ansible/mini-project/ansible/inventory.yml  /home/ansible/mini-project/ansible/install.yml
+
+echo "Running deploy playbook..."
+ansible-playbook -i /home/ansible/mini-project/ansible/inventory.yml  /home/ansible/mini-project/ansible/deploy.yml
+
+
 chmod 600 /home/ansible/.ssh/known_hosts
 echo "ansible master created..."
 exec "$@"
